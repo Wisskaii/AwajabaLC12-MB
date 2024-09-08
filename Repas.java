791f0f9f2a1b7b4456ac22030f83f08287cff72c
Wisskaii <wissam.elsharkawy.sio@gmail.com>
2024-09-08 22:06:28 +0200

    C4

--- Repas.java
+++ Repas.java
@@ -10,4 +10,12 @@ public class Repas {
         this.date = date;
     }
 
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
 }
